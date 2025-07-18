f5d65fb90b03aff00c0fabd35601e8dd3dcb7628
Alphaboy20023 <timivictor565@gmail.com>
2025-07-18 20:04:59 +0200

    tasks not work

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./styles/globals.css";
 import { Providers } from "./providers";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Toaster } from "react-hot-toast";
-import { SessionProvider } from "next-auth/react";
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "taskly",
@@ -31,8 +23,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
           {children}
