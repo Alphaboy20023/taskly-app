b28f5ce4bf6264c85d4ee987cc5007bb065ba59a
Alphaboy20023 <timivictor565@gmail.com>
2025-07-19 09:51:06 +0200

    variable fix

--- src/app/lib/firebase-admin.ts
+++ src/app/lib/firebase-admin.ts
@@ -3,8 +3,8 @@ import { getAuth } from 'firebase-admin/auth';
 
 const firebaseAdminConfig = {
   credential: cert({
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
   })
 };
